7b198da0e1cf7fdf72a00906cc1d08340deed7b5
kaisermann <kaisermann1890@gmail.com>
2024-08-27 09:39:30 -0400

    Se agregaron mas pacientes

--- paciente_estructura.cpp
+++ paciente_estructura.cpp
@@ -9,17 +9,34 @@ struct Paciente {
 };
 
 int main() {
-  cout << "Hola mundo\n";
 
   Paciente paciente1;
-  // Paciente paciente2;
-  // Paciente paciente3;
 
-  paciente1.nombre = "pepe";
+  paciente1.nombre = "Pepe";
   paciente1.edad = 20;
   paciente1.altura = 1.98;
 
+  Paciente paciente2;
+
+  paciente2.nombre = "Rolo";
+  paciente2.edad = 2;
+  paciente2.altura = 0.30;
+
+  Paciente paciente3;
+
+  paciente3.nombre = "Jack";
+  paciente3.edad = 56;
+  paciente3.altura = 1.58;
+
   cout << "el nombre del paciente es: " << paciente1.nombre << "\n";
   cout << "La edad del paciente es: " << paciente1.edad << "\n";
-  cout << "La estatura del paciente es: " << paciente1.altura << "\n";
+  cout << "La estatura del paciente es: " << paciente1.altura << "cm \n";
+
+  cout << "el nombre del paciente es: " << paciente2.nombre << "\n";
+  cout << "La edad del paciente es: " << paciente2.edad << "\n";
+  cout << "La estatura del paciente es: " << paciente2.altura << "cm \n";
+
+  cout << "el nombre del paciente es: " << paciente3.nombre << "\n";
+  cout << "La edad del paciente es: " << paciente3.edad << "\n";
+  cout << "La estatura del paciente es: " << paciente3.altura << "cm \n";
 }
