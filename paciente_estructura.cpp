3d1536e8fe302b879667e3d639217f1f2ce972e0
kaisermann <kaisermann1890@gmail.com>
2024-08-27 09:25:38 -0400

    Se creo la structura y un paciente y se imprimieron sus parametros

--- paciente_estructura.cpp
+++ paciente_estructura.cpp
@@ -2,10 +2,24 @@
 
 using namespace std;
 
-struct Person {
-  std::string nombre;
+struct Paciente {
+  string nombre;
   int edad;
   double altura;
 };
 
-int main() { cout << "Hola mundo\n"; }
+int main() {
+  cout << "Hola mundo\n";
+
+  Paciente paciente1;
+  // Paciente paciente2;
+  // Paciente paciente3;
+
+  paciente1.nombre = "pepe";
+  paciente1.edad = 20;
+  paciente1.altura = 1.98;
+
+  cout << "el nombre del paciente es: " << paciente1.nombre << "\n";
+  cout << "La edad del paciente es: " << paciente1.edad << "\n";
+  cout << "La estatura del paciente es: " << paciente1.altura << "\n";
+}
